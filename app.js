6e64236fdbcfee58e98a93137347443740d0a5e6
lemmyc <mrburgundy2911@gmail.com>
2023-09-07 21:19:09 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -12,6 +12,6 @@ app.get("/", (req, res)=>{
   res.json({message: "Welcome to the contact book application."});
 });
 
-
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
 
