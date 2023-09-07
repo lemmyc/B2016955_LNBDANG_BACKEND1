8ed15b4f2c760b66156c9fe164921c2a18aca491
lemmyc <mrburgundy2911@gmail.com>
2023-09-07 21:40:26 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 
 const app = express();
 
@@ -13,5 +15,16 @@ app.get("/", (req, res)=>{
 });
 
 app.use("/api/contacts", contactsRouter);
+
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
 
